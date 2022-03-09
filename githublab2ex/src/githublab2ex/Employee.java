805b805c6b2038e6736d1ceb25123078e852f003
admin <admin@Bcalab4-m09>
2022-03-09 10:35:42 +0530

    12345

---
+++ githublab2ex/src/githublab2ex/Employee.java
@@ -0,0 +1,13 @@
+package githublab2ex;
+
+public class Employee {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		{
+			System.out.println("Employee Name");
+		}
+
+	}
+
+}
