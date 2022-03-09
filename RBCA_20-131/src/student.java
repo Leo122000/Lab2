071eb0864cab592c7d57dda6d930379395daf5c9
admin <admin@Bcalab4-m09>
2022-03-09 10:29:13 +0530

    1234

--- RBCA_20-131/src/student.java
+++ RBCA_20-131/src/student.java
@@ -1,10 +1,19 @@
 
 public class student {
+	
+	String name;
+	student(String x)
+	{
+		name=x;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+			student a = new student("Alex");
+		
 		{
-			System.out.println("Hello students welcome to git hub.");
+			System.out.println("Hello"+a.name);
 		}
 
 	}
