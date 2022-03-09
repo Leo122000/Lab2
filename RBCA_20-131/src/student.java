9c9a37e8d4ba0b63c6828c313ea13d0837e1f17e
admin <admin@Bcalab4-m09>
2022-03-09 09:17:29 +0530

    commit4

---
+++ RBCA_20-131/src/student.java
@@ -0,0 +1,12 @@
+
+public class student {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		{
+			System.out.println("Hello students welcome to git hub.");
+		}
+
+	}
+
+}
